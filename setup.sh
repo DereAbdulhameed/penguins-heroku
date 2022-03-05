5d68d4f3101c5a032cea854e3c712a58bd2d8bd9
Abdulhameed Dere <77274597+DereAbdulhameed@users.noreply.github.com>
2022-03-05 03:03:35 -0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,9 +1,11 @@
 mkdir -p ~/.streamlit/
 
 echo "\
+[general]\n\
+email = \"daabiola3@gmail.com\"\n\
 [server]\n\
 port = $PORT\n\
 enableCORS = false\n\
 headless = true\n\
 \n\
-" > ~/.streamlit/config.toml
+" > ~/.streamlit/credentials.toml
